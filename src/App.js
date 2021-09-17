0eb3f116d3c0458de05964b9e35918cb0a48155d
unknown <you@example.com>
2021-09-17 23:17:16 +0400

    added bg

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 
 const api = {
   key: "2cdf06a36c452536f181154cb6374893",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
   const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-  
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+
     let day = "3";
-    console.log(day)
-  }
+    console.log(day);
+  };
   return (
     <div className="app">
-       <div className="container">Hello</div>
+      <main className="main">
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="search" />
+        </div>
+      </main>
     </div>
   );
 }
