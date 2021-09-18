859847db16bcd7fcaa4c01357b5ba4fabc3eb5e6
unknown <you@example.com>
2021-09-18 23:43:42 +0400

    added some styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
+let request = fetch('api.openweathermap.org/data/2.5/weather?q=London&appid=2cdf06a36c452536f181154cb6374893')
+console.log(request)
+
 function App() {
   const dateBuilder = (d) => {
     let months = [
@@ -23,23 +26,35 @@ function App() {
       "December",
     ];
     let days = [
+      "Sunday",
       "Monday",
       "Tuesday",
       "Wednesday",
       "Thursday",
       "Friday",
       "Saturday",
-      "Sunday",
     ];
 
-    let day = "3";
-    console.log(day);
+    let day = days[d.getDay()],
+        date = d.getDate(),
+        month = months[d.getMonth()],
+        year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
   };
   return (
     <div className="app">
       <main className="main">
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="search" />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15˚C</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
